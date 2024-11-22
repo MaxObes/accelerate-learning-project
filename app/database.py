9732f84043a33fc1ec6dc24fdfc9d763abf03827
Maxwell Oberbrunner <maxobes12@gmail.com>
2024-11-21 20:26:50 -0600

    Database is running and can access GraphQL - working on testing API endpoints;

--- app/database.py
+++ app/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"  # Or your database URL
+# PostgreSQL database URL
+DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/postgres"
 
 # SQLAlchemy engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 # Base class for models
 Base = declarative_base()
